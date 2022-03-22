0c87f4031bad8f9c0ae40a9e6aacd3ed01eb92dd
Mike Stone <michael.douglas.stone@gmail.com>
2022-03-22 11:26:28 -0500

    Update 'rclone_borg_sync.sh'

--- rclone_borg_sync.sh
+++ rclone_borg_sync.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-repos=( lb pihole proxmox proxmox-b proxmox-c home )
+repos=( rpool/ROOT/pve-1 backup/proxmox-backup-server tank/file-cabinet tank/keepsake tank/keepsake2 tank/mikesclassicads tank/pbs tank/personal tank/testing tank/timemachine )
 bwlimit=1024k
+/backup/borg/backup/proxmox-backup-server/
 
 #Bail if rclone is already running, maybe previous run didn't finish
 if pidof -x rclone >/dev/null; then
@@ -13,21 +14,21 @@ for i in "${repos[@]}"
 do
     #Lets see how much space is used by directory to back up
     #if directory is gone, or has gotten small, we will exit
-    space=`du -s /tank/backups/borg/$i|awk '{print $1}'`
+    space=`du -s /backup/borg/$i|awk '{print $1}'`
 
     if (( $space < 4500 )); then
        echo "not enough space used in $i ($space) - skipping!"
     else
        echo "==================== syncing $i"
-       /usr/bin/rclone --config /root/.config/rclone/rclone.conf -v sync /tank/backups/borg/$i b2-borg:/billimek-borg/$i
+       /usr/bin/rclone --config /home/mike/.config/rclone/rclone.conf -vvv sync --dry-run /backup/borg/$i b2:mds-borgbackup/$i
     fi
 
 done
 
-echo "==================== syncing RESTIC backups"
-restic_repos=( default monitoring )
-for i in "${restic_repos[@]}"
-do
-  echo "==================== syncing $i"
-  /usr/bin/rclone --config /root/.config/rclone/rclone.conf -v sync /tank/data/minio/velero/restic/$i b2-restic:/billimek-restic/restic/$i
-done
+#echo "==================== syncing RESTIC backups"
+#restic_repos=( default monitoring )
+#for i in "${restic_repos[@]}"
+#do
+#  echo "==================== syncing $i"
+#  /usr/bin/rclone --config /root/.config/rclone/rclone.conf -v sync /tank/data/minio/velero/restic/$i b2-restic:/billimek-restic/restic/$i
+#done
